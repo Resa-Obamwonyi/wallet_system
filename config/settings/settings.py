d61aaf548ea09e6c82492f4089fa52eb0e90632b
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2021-01-11 23:59:36 +0100

    Set up CI/CD

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-print(config('SECRET_KEY'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
