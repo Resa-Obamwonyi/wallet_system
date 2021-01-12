a5760390e358aec2df89a8247f65af08c013f02c
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2021-01-12 12:31:46 +0100

    Set up Models

--- walletsystem/wallet/models.py
+++ walletsystem/wallet/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser
+
+import uuid
+
 
 # Create your models here.
+class User(AbstractBaseUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    firstname = models.CharField(max_length=100, null=False)
+    lastname = models.CharField(max_length=100, null=False)
+    email = models.CharField(max_length=255, null=False)
+    password = models.CharField(max_length=255)
+    is_admin = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=False, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Elite(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    wallet_type = models.CharField(max_length=100, null=False)
+    main_currency = models.CharField(max_length=100, null=False)
+    created_at = models.DateTimeField(auto_now=False, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Noob(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    wallet_type = models.CharField(max_length=100, null=False)
+    main_currency = models.CharField(max_length=100, null=False)
+    created_at = models.DateTimeField(auto_now=False, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Wallet(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=100, null=False)
+    balance = models.CharField(max_length=255, null=False)
+    main = models.BooleanField()
+    created_at = models.DateTimeField(auto_now=False, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Transactions(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    wallet_id = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=100, null=False)
+    amount = models.CharField(max_length=255, null=False)
+    currency = models.CharField(max_length=100, null=False)
+    status = models.CharField(max_length=100, null=False)
+    created_at = models.DateTimeField(auto_now=False, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
