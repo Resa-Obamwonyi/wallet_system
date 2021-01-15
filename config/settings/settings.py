f060b84c6a40129100ce8b9f90391feeab39554c
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2021-01-15 15:59:50 +0100

    heroku db bug with default db both

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -83,8 +83,9 @@ AUTH_USER_MODEL = 'wallet.User'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-if not DEBUG:
+if DEBUG is False:
     DATABASES = {'default': dj_database_url.config(conn_max_age=60)}
+    print(config("DATABASE_URL"))
 else:
     DATABASES = {
         'default': {
